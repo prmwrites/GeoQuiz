593175e37fdfec02f7e5bbc336c298aa8d2c9f98
Patrick Morris <prmwrites@gmail.com>
2016-06-16 00:46:19 -0400

    added button listeners to QuizActivity

--- app/src/main/java/com/grandroid/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/grandroid/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.grandroid.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
@@ -15,6 +16,19 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v) {
+                // Does nothing yet, but will soon
+            }
+        });
+
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Does nothing yet, but will soon
+            }
+        });
     }
 }
