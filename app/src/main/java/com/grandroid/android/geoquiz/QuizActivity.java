4fb5e4f34cfc9d33ac88c87b45452557c3595f6a
Patrick Morris <prmwrites@gmail.com>
2016-06-16 01:16:52 -0400

    Added Toast messages to true and false Button clicks

--- app/src/main/java/com/grandroid/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/grandroid/android/geoquiz/QuizActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -19,7 +21,7 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
                     public void onClick(View v) {
-                // Does nothing yet, but will soon
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -27,7 +29,7 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                // Does nothing yet, but will soon
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
             }
         });
     }
